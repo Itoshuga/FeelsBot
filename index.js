e91274070959d698ea3e60dc0bf0ed66f2d1aa02
XX5 <wagashisakatsuki@gmail.com>
2023-04-30 03:43:12 +0200

    feat: add an handling system for events

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
+const { loadEvents } = require('./Handlers/eventHandler');
+
 const client = new Client({
     intents: [Guilds, GuildMembers, GuildMessages],
     partials: [User, Message, GuildMember, ThreadMember],
 });
 
-client.once('ready', () => {
-    console.log("Ready!")
-})
-
 client.config = require('./config.json')
 
-client.login(client.config.token);
+client.login(client.config.token).then(() => {
+    loadEvents(client);
+});
