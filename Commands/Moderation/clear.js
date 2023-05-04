54d3f852bb1c85348aaf00c114cc873b5dedfd47
XX5 <wagashisakatsuki@gmail.com>
2023-05-04 21:43:21 +0200

    fix: bug when you set an amount > 100

--- Commands/Moderation/clear.js
+++ Commands/Moderation/clear.js
@@ -1,31 +1,37 @@
-const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBit, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 module.exports = {
-    data:  new SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName('clear')
-        .setDescription('Supprime un nombre de message définit.')
-        .setDefaultMemberPermissions(PermissionFlagsBit.ManageMessage)
-        .addIntegerOption(option => 
+        .setDescription('Removes a defined number of messages.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addIntegerOption(option =>
             option.setName('amount')
-            .setDescription('Amount')
-            .setRequired(true)
-            )
+                .setDescription('Amount of messages to delete.')
+                .setRequired(true)
+        )
         .addUserOption(option =>
             option.setName('target')
-            .setDescription('Choisit la personne a ciblé')
-            .setRequired(true)
-            ),
+                .setDescription('Target whose messages will be deleted.')
+                .setRequired(false)
+        ),
     async execute(interaction) {
         const { channel, options } = interaction;
 
         const amount = options.getInteger('amount');
         const target = options.getUser('target');
 
+        const clearEmbed = new EmbedBuilder()
+
+        if (amount > 100) {
+            clearEmbed.setColor(0xff0000)
+            clearEmbed.setDescription(`Sorry, it's impossible to delete more than 100 messages.`);
+            interaction.reply({ embeds: [clearEmbed]});
+            return;
+        }
+
         const messages = await channel.messages.fetch({
             limit: amount + 1,
         })
-
-        const clearEmbed = new EmbedBuilder()
-            .setColor(0x5fb041)
 
         if (target) {
             let i = 0;
@@ -39,13 +45,15 @@ module.exports = {
             });
 
             await channel.bulkDelete(filtered).then(messages => {
+                clearEmbed.setColor(0x5fb041)
                 clearEmbed.setDescription(`Successfully deleted ${messages.size} messages from ${target}.`);
-                message.reply({ embeds: [clearEmbed]});
+                interaction.reply({ embeds: [clearEmbed]});
             });
         } else {
             await channel.bulkDelete(amount, true).then(messages => {
+                clearEmbed.setColor(0x5fb041)
                 clearEmbed.setDescription(`Successfully deleted ${messages.size} messages from the channel.`);
-                message.reply({ embeds: [clearEmbed]});
+                interaction.reply({ embeds: [clearEmbed]});
             })
         }
     }
